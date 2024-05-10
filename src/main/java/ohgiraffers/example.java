56cf9d4566aabaddb6afa8c8eda984f52184fb13
kimkinghyeon <modan8199@gmail.com>
2024-05-10 11:09:52 +0900

    first commit 간단하게 변경한 내용들
    출력 문자 변경
    
    자세하게 설명

--- src/main/java/ohgiraffers/example.java
+++ src/main/java/ohgiraffers/example.java
@@ -4,7 +4,7 @@ public class example {
     public static void main(String[] args) {
 
       System.out.print("hello world!");
-      System.out.print("한글은 안깨져요");
+      System.out.print("아 졸리다 자고싶다");
 
    }
 }
